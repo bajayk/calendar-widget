1a78158a9821fede6fb614da24ed5bba2b363f06
Ajay Badgujar <bajayk@gmail.com>
2019-10-20 22:49:24 +0530

    Minor updates

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/**
+ * Author Name: Ajay Badgujar
+ * Website: https://www.ajaybadgujar.com/
+ * Youtube: https://www.youtube.com/channel/UCCnj0pD6f90B3ljdw98M7jA
+ */
+
 import Calendar from './calendar.class';
 
 var input1 = new Calendar({id:"input-data-1"});
